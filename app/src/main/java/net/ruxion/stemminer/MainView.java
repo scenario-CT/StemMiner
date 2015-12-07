7e9e96c93054af555ddacfb52888642899052ba2
townsendc <townsendc@bentonvillek12.org>
2015-12-07 12:04:12 -0600

    some stuff

--- app/src/main/java/net/ruxion/stemminer/MainView.java
+++ app/src/main/java/net/ruxion/stemminer/MainView.java
@@ -2,25 +2,66 @@ package net.ruxion.stemminer;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 
-/**
- * Created by townsendc on 12/2/15.
- */
 public class MainView extends View
 {
+	private static Paint paint = new Paint();
+	private static int height;
+	private static int width;
+
+
 	public MainView(Context context, AttributeSet attrs)
 	{
 		super(context, attrs);
+
+		Display display = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		width = size.x;
+		height = size.y;
+
+		System.out.println(width+"x"+height);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas)
 	{
-		// hey dog when you read this this thing is supposed to be like alot done by christamas break
-		// so yeah
+
+		Drawable background = getResources().getDrawable(R.drawable.space);
+		background.setBounds(0, 0, width, height);
+		background.draw(canvas);
+
+
+
+	}
+
+	class Ship
+	{
+		boolean on;
+		int postion;
+
+		public Ship()
+		{
+			on = false;
+			postion = 0;
+		}
+
+		public void draw(Canvas canvas)
+		{
+			Drawable ship = getResources().getDrawable(R.drawable.spaceship);
+			ship.setBounds(0, 0, width, height);
+			ship.draw(canvas);
+
+
+		}
 	}
 
 	@Override
